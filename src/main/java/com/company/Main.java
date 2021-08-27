765c9518d544675729b938d8d38ccd5c1b2c0574
AlijaJeniceka <alija.juldaseva@gmail.com>
2021-08-27 20:34:07 +0300

    Initial commit

---
+++ src/main/java/com/company/Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+import com.company.Game.Login;
+
+public class Main {
+    public static void main(String[] args) {
+      Login.main();
+     //   Registration.main();
+       // CowsAndBulls.main();
+    }
+}
